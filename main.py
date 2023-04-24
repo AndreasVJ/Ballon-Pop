63b8accc65c022d84fc3ec0b84b402025fe18d82
Andreas Jonsterhaug <andreas.vj@icloud.com>
2023-04-24 22:36:40 +0200

    added Game class

--- main.py
+++ main.py
@@ -1,77 +1,97 @@
 import pygame
 import sys
-from balloon import Balloon, generateRandomBalloon
+from balloon import Balloon, generate_random_balloon
 from text import draw_text
-from globals import FPS, WIDTH, HEIGHT
+from globals import FPS, WIDTH, HEIGHT, BALLON_ASPECT_RATIO
 
 
 pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-font32 = pygame.font.Font('freesansbold.ttf', 32)
-font64 = pygame.font.Font('freesansbold.ttf', 64)
-pygame.display.set_caption('Balloon pop')
+font32 = pygame.font.Font("freesansbold.ttf", 32)
+font64 = pygame.font.Font("freesansbold.ttf", 64)
+pygame.display.set_caption("Balloon Pop")
 
 
-game_time = 12 * 1000
-max_time = game_time
-score = 0
-balloons: list[Balloon] = []
-for i in range(10):
-    balloons.append(generateRandomBalloon())
+class Game:
+    def __init__(self, screen, game_time, num_balloons):
+        self.screen = screen
+        self.game_time = game_time * 1000
+        self.max_time = self.game_time
+        self.time = 0
+        self.score = 0
+        self.balloons: list[Balloon] = []
+        for i in range(num_balloons):
+            self.balloons.append(generate_random_balloon())
 
 
-while True:
-    # Get elapsed time
-    time = pygame.time.get_ticks()
-    dt = clock.tick(FPS)
-
-    # Clear previous frame
-    screen.fill((0, 0, 0))
-
-    # Read input from user
-    for event in pygame.event.get():
+    def handle_input(self, event: pygame.event.Event):
         if event.type == pygame.QUIT:
             sys.exit()
 
+        # Check if player pressed keyboard
         if event.type == pygame.KEYDOWN:
             match event.key:
                 case pygame.K_ESCAPE:
                     sys.exit()
                 
+                # Reset all game states
                 case pygame.K_r:
-                    score = 0
-                    max_time = time + game_time
-                    balloons = []
+                    self.max_time = self.time + self.game_time
+                    self.score = 0
+                    self.balloons = []
                     for i in range(10):
-                        balloons.append(generateRandomBalloon())
+                        self.balloons.append(generate_random_balloon())
         
         # Check if player clicked on a balloon
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-            for i, b in enumerate(balloons):
-                if b.is_inside(x, y):
-                    balloons[i] = generateRandomBalloon()
-                    score += 1
-                    
-    
-    # Check if time is used up
-    if time > max_time:
-        draw_text(screen, "Game Over", font64, 300, 300)
-        draw_text(screen, f'score: {score}', font32, 300, 364)
-        draw_text(screen, "Press 'r' to try again", font32, 300, 396)
+            for i, balloon in enumerate(self.balloons):
+                if balloon.is_inside(x, y):
+                    self.balloons[i] = generate_random_balloon()
+                    self.score += 1
 
-    else:
+
+    def update(self):
+        # Get elapsed time
+        self.time = pygame.time.get_ticks()
+        dt = clock.tick(FPS)
+
         # Update balloons
-        for i, b in enumerate(balloons):
-            b.update(dt)
-            if b.pos.y < - b.size:
-                balloons[i] = generateRandomBalloon()
-            b.draw(screen)
+        for i, balloon in enumerate(self.balloons):
+            balloon.update(dt)
+            if balloon.pos.y < - balloon.size*BALLON_ASPECT_RATIO:
+                self.balloons[i] = generate_random_balloon()
+    
 
+    def draw(self):
+        # Clear previous frame
+        screen.fill((0, 0, 0))
+
+        # Check if time is used up
+        if self.time > self.max_time:
+            draw_text(screen, "Game Over", font64, 0, 0)
+            draw_text(screen, f'score: {self.score}', font32, 300, 364)
+            draw_text(screen, "Press 'r' to try again", font32, 300, 396)
         
-        # Draw time and score
-        draw_text(screen, f'time left: {(max_time - time) // 1000}', font32, 16, 16)
-        draw_text(screen, f'score: {score}', font32, 16, 48)
+        else:
+            # Draw balloons
+            for b in self.balloons:
+                b.draw(screen)
 
-    pygame.display.flip()
+            # Draw time and score
+            draw_text(screen, f'time left: {(self.max_time - self.time) // 1000}', font32, 16, 16)
+            draw_text(screen, f'score: {self.score}', font32, 16, 48)
+
+        pygame.display.flip()
+
+
+game = Game(screen, 60, 10)
+
+while True:
+    # Read input from user
+    for event in pygame.event.get():
+        game.handle_input(event)
+    
+    game.update()
+    game.draw()
